76847f04ed70387771163dfbad9ad050405668a8
Carlos O <142923249+oropeval@users.noreply.github.com>
2024-10-28 12:48:02 -0600

    first commit

---
+++ src/main/java/com/sistema_facturacion/sistema_de_facturacion/SistemaDeFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_facturacion.sistema_de_facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeFacturacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_facturacion/sistema_de_facturacion/SistemaDeFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_facturacion.sistema_de_facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
